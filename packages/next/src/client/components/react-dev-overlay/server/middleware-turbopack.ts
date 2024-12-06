707756ffbf82cc707f010822ec3d09ce515defdc
Tobias Koppers <tobias.koppers@googlemail.com>
2024-12-06 08:48:30 +0100

    add ./ to relative paths to make it easier to understand

--- packages/next/src/client/components/react-dev-overlay/server/middleware-turbopack.ts
+++ packages/next/src/client/components/react-dev-overlay/server/middleware-turbopack.ts
@@ -19,6 +19,7 @@ import type { Project, TurbopackStackFrame } from '../../../../build/swc/types'
 import { getSourceMapFromFile } from '../internal/helpers/get-source-map-from-file'
 import { findSourceMap, type SourceMapPayload } from 'node:module'
 import { pathToFileURL } from 'node:url'
+import { isAbsolute } from 'node:path'
 
 function shouldIgnorePath(modulePath: string): boolean {
   return (
@@ -255,8 +256,13 @@ async function nativeTraceSource(
 
 function relativeToCwd(file: string): string {
   const relPath = path.relative(process.cwd(), url.fileURLToPath(file))
-  // TODO(sokra) include a ./ here to make it a relative path
-  return relPath
+  if (isAbsolute(relPath)) {
+    return relPath
+  }
+  if (relPath.startsWith('../')) {
+    return relPath
+  }
+  return './' + relPath
 }
 
 export async function createOriginalStackFrame(
